9e00aad411d9c15dd57cd020af168bc4648d8263
Amberley Romo <amberleyjohanna@gmail.com>
2018-08-12 13:26:56 -0700

    move global css

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 import { Header } from './'
-import './layout.css'
 
 const Layout = ({ children, data }) => (
   <StaticQuery
